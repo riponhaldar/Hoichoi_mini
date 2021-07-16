b21eae8793681c77c3108df3a5cd5fba79128d0a
Ripon Haldar <mackparker001@gmail.com>
2021-07-16 15:27:26 +0530

    all done

--- js/app.js
+++ js/app.js
@@ -22,3 +22,43 @@ $('.single-item').slick({
 	// 	'  <span class="sr-only">Next slide</span>' +
 	// 	'</button>',
 });
+
+// end slide
+
+$('.addedslide').slick({
+	// infinite: false,
+	speed: 300,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	nextArrow: $('.next1'),
+	prevArrow: $('.prev1'),
+
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			},
+		},
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	],
+});
